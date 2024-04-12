70b4965ad4f717322bcddb33bacc3d5f2f7255d6
pandaloro <136196122+pandaloro@users.noreply.github.com>
2024-04-12 16:03:02 -0400

    Update contact-view.php

--- public/assets/views/main/contact-view.php
+++ public/assets/views/main/contact-view.php
@@ -15,7 +15,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
 
