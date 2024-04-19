400ebf96fba2fa167dff13d4d7214a59bdcd4e75
pandaloro <136196122+pandaloro@users.noreply.github.com>
2024-04-19 13:06:58 -0400

    Update Model.php

--- app/core/Model.php
+++ app/core/Model.php
@@ -11,7 +11,12 @@ Trait Model
         $query = "select * from $this->table";
         return $this->query($query);
     }
+    public function post($inputData)
+    {
+        $query = "insert into $this->table (name, email, phone, description) values (:name, :email, :phone, :description);";
+        return $this->query($query, $inputData);
 
+    }
 
 
 }
