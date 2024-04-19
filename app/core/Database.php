57810baa0c12d41cf896fc7230d3a64e464d9780
pandaloro <136196122+pandaloro@users.noreply.github.com>
2024-04-19 13:56:34 -0400

    Update Database.php

--- app/core/Database.php
+++ app/core/Database.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace app\core;
+use PDO;
+use PDOException;
 
 Trait Database
 {
@@ -28,7 +30,25 @@ Trait Database
 
         return false;
     }
+    public function queryWithParams($query, $data, $className = null) {
+        $connectedPDO = $this->connect();
+
+        $statementObject = $connectedPDO->prepare($query);
+        $statementObject->execute($data);
+
+        if ($className) {
+            $statementObject->setFetchMode(PDO::FETCH_CLASS, $className);
+        }
+
+        $result = $statementObject->fetchAll();
+        if (is_array($result) && count($result)) {
+            return $result;
+        }
+
+        return false;
+    }
 
 }
+
 
 
