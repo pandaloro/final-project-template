7a81f4aec413516cf51b5abe8fd13aa7248629a0
pandaloro <136196122+pandaloro@users.noreply.github.com>
2024-04-19 14:05:49 -0400

    Update MainController.php

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,9 +10,9 @@ class MainController extends Controller
     public function homepage()
     {
                 include '../public/assets/views/main/homepage.php';
-//        $this->view('../public/assets/views/main/example-homepage.php', true);
+        //$this->view('../public/assets/views/main/homepage.php', true);
     }
-
+    
     public function notFound()
     {
     }
